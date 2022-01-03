f1ab7284534802ba9829ace1b7201c1878d8a814
Het Desai <hetdesai068@gmail.com>
2022-01-03 21:21:09 +0530

    BackEnd Initialized

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import { hashSync, compareSync } from 'bcrypt';
+import mongoose from 'mongoose';
+import env from "dotenv";
+import passport from 'passport'
+import jwt from 'jsonwebtoken'
+
+
+
+const app = express();
+app.use(cors());
+env.config({ path: ".env" })
+const uri = process.env.ATLAS_URI
+app.use(express.json())
+app.use(passport.initialize())
+
+// mongoose.connect(uri);
+// mongoose.connection.once("open", () => {
+//     console.log("MongoDB conncetion established !!")
+// })
+
+
+app.listen(4000, () => {
+    console.log("Server started on port - 4000 !! -----------------------------------------------------------------")
+})
